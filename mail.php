437b06b4f97493a87f65504dbd38c91353d35a09
idadrusdiana <idadrusdiana01@gmail.com>
2021-01-20 10:37:37 +0700

    Email changed to yoyaku.id

--- mail.php
+++ mail.php
@@ -26,15 +26,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'mail.yoyaku.id';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'no-reply@yoyaku.id';                     // SMTP username
+    $mail->Username   = 'sumrochkaryaindonesia@gmail.com';                     // SMTP username
     $mail->Password   = '@Jalanpaskal23';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('no-reply@gmail.com', 'Yoyaku');
+    $mail->setFrom('sumrochkaryaindonesia@gmail.com', 'Sumroch');
     $mail->addAddress('sumroch.ki@gmail.com', 'Sumroch');     // Add a recipient
     $mail->addCC('sri@yoyaku.id');
     $mail->addCC('agunbuhori@gmail.com');
