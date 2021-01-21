4064b3cd4077c7f57ca7399ab99ff5c6bba2e175
idadrusdiana <idadrusdiana01@gmail.com>
2021-01-21 14:56:07 +0700

    ganti icon

--- mail.php
+++ mail.php
@@ -28,16 +28,15 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'sumrochkaryaindonesia@gmail.com';                     // SMTP username
-    $mail->Password   = '@Jalanpaskal23';                               // SMTP password
+    $mail->Username   = 'yoyaku.id@gmail.com';                     // SMTP username
+    $mail->Password   = 'Jalanpaskal1';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('yoyaku.id@gmail.com', 'Yoyaku System');
     $mail->addAddress('sri@yoyaku.id', 'Yoyaku System');     // Add a recipient
-    $mail->addCC('sri@yoyaku.id');
-    $mail->addCC('agunbuhori@gmail.com');
+    $mail->addCC('sri@yoyaku.id');    
     $mail->addCC('idadrusdiana01@gmail.com');
 
     $jenis_rental = implode(', ', $_POST['jenis_rental']);
